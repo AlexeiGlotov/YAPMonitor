dab5210c578d27e39a13ca46b8d562775f4c0787
Alexei Windsor <tnffx@Alexeis-MacBook-Pro.local>
2024-06-29 20:44:45 +0300

    test test

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"io"
+	"net/http"
+)
+
+type gauge struct {
+}
+
+type counter struct {
+}
+
+func updateMetrics(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello, world!\n")
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/update/", updateMetrics)
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
